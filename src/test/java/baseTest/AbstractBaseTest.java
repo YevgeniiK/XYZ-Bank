18cf81cb7611964c57dcfd00fe5658e6d229fc48
Oleksii Venedchuk <oleksijvenedchuk@gmail.com>
2023-06-02 16:26:24 +0300

    AbstractBasetestClassCreating6

--- src/test/java/baseTest/AbstractBaseTest.java
+++ src/test/java/baseTest/AbstractBaseTest.java
@@ -1,14 +1,12 @@
 package baseTest;
 
-import com.codeborne.selenide.Selenide;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 public abstract class AbstractBaseTest {
 
-    @BeforeClass
+    @BeforeSuite
     public static void setUp() {
         String activeProfile = System.getProperty("activeProfile", "local");
         DriverFactory.setUpBrowser(activeProfile);
-        Selenide.open("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
     }
 }
