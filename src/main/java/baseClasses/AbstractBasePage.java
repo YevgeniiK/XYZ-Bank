57d2018559a56ef17c2b203c8bdd91c8fda690c4
Oleksii Venedchuk <oleksijvenedchuk@gmail.com>
2023-06-05 16:05:14 +0300

    Setup BasePage

--- src/main/java/baseClasses/AbstractBasePage.java
+++ src/main/java/baseClasses/AbstractBasePage.java
@@ -1,17 +1,10 @@
 package baseClasses;
 
 import static com.codeborne.selenide.Selenide.open;
+
 public abstract class AbstractBasePage {
 
-    public void openHomePage() {
-        open("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-    }
-
-    public void openCustomerPage() {
-        open("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/customer");
-    }
-
-    public void openManagerPage() {
-        open("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager");
+    public void openURL(String URL) {
+        open(URL);
     }
 }
