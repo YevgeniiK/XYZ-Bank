ecb8f2a35743842a7e79574a42166381e107e113
Oleksii Venedchuk <oleksijvenedchuk@gmail.com>
2023-06-06 12:37:05 +0300

    QAP-11_bankManagerAccountButtonTest and test classes structure creation

--- src/main/java/baseClasses/HomePage.java
+++ src/main/java/baseClasses/HomePage.java
@@ -1,7 +1,11 @@
 package baseClasses;
 
 import com.codeborne.selenide.SelenideElement;
+
+
 import static com.codeborne.selenide.Selenide.$x;
+
+
 
 public class HomePage extends AbstractBasePage{
 
@@ -9,4 +13,13 @@ public class HomePage extends AbstractBasePage{
     private static SelenideElement btnBankManagerLogin = $x("//button [@ng-click='manager()']");
     private static SelenideElement btnHome = $x("//button [@ng-click='home()']");
     private static SelenideElement fieldHeading = $x("//strong [@class='mainHeading']");
+
+    public HomePage open() {
+        openURL("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+        return this;
+    }
+
+    public void bankManagerLoginButtonClick(){
+        btnBankManagerLogin.click();
+    }
 }
