fec9e2fccfc191feb162076d1f01f7963e86f062
Oleksii Venedchuk <oleksijvenedchuk@gmail.com>
2023-06-07 17:26:56 +0300

    QAP-26_AddCustomerButtonTest

--- src/main/java/baseClasses/BankManagerLoginPage.java
+++ src/main/java/baseClasses/BankManagerLoginPage.java
@@ -2,15 +2,18 @@ package baseClasses;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$x;
+import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class BankManagerLoginPage extends AbstractBasePage {
 
     private static SelenideElement addCustomerLoginBtn = $x("//button[normalize-space()='Add Customer']");
 
-    public void addCustomerLoginButtonClick() {
-        addCustomerLoginBtn.click();
+    public BankManagerLoginPage addCustomerLoginButtonClick() {
+        addCustomerLoginBtn.shouldBe(visible, Duration.ofSeconds(3)).click();
+        return this;
     }
 }
 
--- src/test/java/tests/bankManager/BankManagerAccountTests.java
+++ src/test/java/tests/bankManager/BankManagerAccountTests.java
@@ -3,47 +3,43 @@ package tests.bankManager;
 import baseClasses.BankManagerLoginPage;
 import baseClasses.HomePage;
 import baseTest.AbstractBaseTest;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Selenide.sleep;
-import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class BankManagerAccountTests extends AbstractBaseTest {
 
     private HomePage homePage;
     private BankManagerLoginPage bankManagerLoginPage;
+    private Assertions assertions;
 
 
     @BeforeMethod
     public void openBankHomePage() {
         homePage = new HomePage();
         bankManagerLoginPage = new BankManagerLoginPage();
+        assertions = new Assertions();
         homePage.open();
     }
 
     @Test
     public void bankManagerAccountButtonTest() {
         homePage.bankManagerLoginButtonClick();
-        sleep(3000);
-        Assert.assertTrue(url().contains("manager"));
+        assertions.assertUrlContains("manager");
     }
 
     @Test
     public void homePageButtonTest() {
-        homePage.bankManagerLoginButtonClick();
-        homePage.homePageButtonClick();
-        sleep(3000);
-        Assert.assertTrue(url().contains("login"));
+        homePage
+                .bankManagerLoginButtonClick()
+                .homePageButtonClick();
+        assertions.assertUrlContains("login");
     }
 
     @Test
     public void addCustomerButtonTest() {
         homePage.bankManagerLoginButtonClick();
         bankManagerLoginPage.addCustomerLoginButtonClick();
-        sleep(3000);
-        Assert.assertTrue(url().contains("addCust"));
+        assertions.assertUrlContains("addCust");
     }
 
 }
