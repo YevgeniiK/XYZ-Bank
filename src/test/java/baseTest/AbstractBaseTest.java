e60de8726c3fc5593220ca9bbcb35ae521bd0aa3
Serhii Hirchenko <girchenko1979@ukr.net>
2023-06-12 11:17:09 +0300

    QAP-30

--- src/test/java/baseTest/AbstractBaseTest.java
+++ src/test/java/baseTest/AbstractBaseTest.java
@@ -1,5 +1,6 @@
 package baseTest;
 
+import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeSuite;
 
 public abstract class AbstractBaseTest {
@@ -7,6 +8,7 @@ public abstract class AbstractBaseTest {
     @BeforeSuite
     public static void setUp() {
         String activeProfile = System.getProperty("activeProfile", "local");
-//        DriverFactory.setUpBrowser(activeProfile);
+        DriverFactory.setUpBrowser(activeProfile);
+        Configuration.browserSize = "1920x1080";
     }
 }
