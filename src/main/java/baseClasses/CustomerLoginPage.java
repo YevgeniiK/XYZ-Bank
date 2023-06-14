82b3b86be2fcac359658c0cee0d524b6e851d48a
IhorHerman <rogigerman@gmail.com>
2023-06-14 12:31:42 +0300

    QAP_14 created

--- src/main/java/baseClasses/CustomerLoginPage.java
+++ src/main/java/baseClasses/CustomerLoginPage.java
@@ -6,8 +6,9 @@ import static com.codeborne.selenide.Selenide.$x;
 public class CustomerLoginPage extends AbstractBasePage {
 
     private static SelenideElement fieldUserSelect = $x("//select[@name='userSelect']");
-    private static SelenideElement selectUser = $x("//option[@value='3']");
+    private static SelenideElement selectUser = $x("//option[@value='6']");
     private static SelenideElement btnLogin = $x("//button[@type='submit']");
+    private static SelenideElement btnLogout = $x("//button[@class='btn logout']");
     private static SelenideElement btnWithdrawl = $x("//button[@ng-class='btnClass3']");
 
     public CustomerLoginPage clickFieldSelectUserName() {
@@ -25,6 +26,12 @@ public class CustomerLoginPage extends AbstractBasePage {
     public CustomerLoginPage clickBtnLogin() {
         logger.info("Click button Login");
         btnLogin.click();
+        return this;
+    }
+
+    public CustomerLoginPage clickBtnLogout() {
+        logger.info("Click button Logout");
+        btnLogout.click();
         return this;
     }
 
