e88520fe33ad9ee47dd1fcb2dff0eea13e420654
Maksym <maksimkoval639@gmail.com>
2023-06-19 17:47:50 +0300

    QAP-19

--- src/main/java/baseClasses/CustomerLoginPage.java
+++ src/main/java/baseClasses/CustomerLoginPage.java
@@ -9,6 +9,10 @@ public class CustomerLoginPage extends AbstractBasePage {
     private static SelenideElement selectUser = $x("//option[@value='3']");
     private static SelenideElement btnLogin = $x("//button[@type='submit']");
     private static SelenideElement btnWithdrawl = $x("//button[@ng-class='btnClass3']");
+    private static SelenideElement btnDeposit = $x("//button[@ng-class = 'btnClass2']");
+    private static SelenideElement confirmDepositBtn = $x("//button[normalize-space() = 'Deposit' and @type]");
+    private static SelenideElement amountField = $x("//input[@ng-model = 'amount']");
+    private static SelenideElement depositSuccessfulMessage = $x("//span[normalize-space() = 'Deposit Successful']");
 
     public CustomerLoginPage clickFieldSelectUserName() {
         logger.info("Click field select UserName");
@@ -38,4 +42,34 @@ public class CustomerLoginPage extends AbstractBasePage {
         btnWithdrawl.click();
         return this;
     }
+
+    public CustomerLoginPage clickBtnDeposit(){
+        btnDeposit.click();
+        return this;
+    }
+
+    public CustomerLoginPage clickBtnDepositConfirm(){
+        confirmDepositBtn.click();
+        return this;
+    }
+
+    public CustomerLoginPage clickAmountField(){
+        amountField.click();
+        return this;
+    }
+
+    public SelenideElement getDepositSuccessfulMessageText(){
+        return depositSuccessfulMessage;
+    }
+
+    public CustomerLoginPage selectUserByName(String text){
+        fieldUserSelect.selectOptionContainingText(text);
+        return this;
+    }
+
+    public CustomerLoginPage fillAmountField(Integer number){
+        amountField.setValue(String.valueOf(number)).pressEnter();
+        return this;
+    }
+
 }
