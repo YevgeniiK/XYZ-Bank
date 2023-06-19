96efe19630f6d6c93e37d2ae7917553db8b0074a
Maksym <maksimkoval639@gmail.com>
2023-06-19 14:51:45 +0300

    Test of customer registration with different currency

--- src/main/java/baseClasses/BankManagerLoginPage.java
+++ src/main/java/baseClasses/BankManagerLoginPage.java
@@ -10,9 +10,77 @@ import static com.codeborne.selenide.Selenide.$x;
 public class BankManagerLoginPage extends AbstractBasePage {
 
     private static SelenideElement addCustomerLoginBtn = $x("//button[normalize-space()='Add Customer']");
+    private static SelenideElement firstNameField = $x("//input[contains (@placeholder, 'First Name')]");
+    private static SelenideElement lastNameField = $x("//input[contains (@placeholder, 'Last Name')]");
+    private static SelenideElement postCodeField = $x("//input[contains (@placeholder, 'Post Code')]");
+    private static SelenideElement addCustomerConfirmButton = $x("//button[@type= 'submit' and normalize-space()= 'Add Customer']");
+    private static SelenideElement openAccountButton = $x("//button[@ng-click = 'openAccount()']");
+    private static SelenideElement selectCustomerField = $x("//select[@name ='userSelect']");
+    private static SelenideElement selectCurrency = $x("//select[@name ='currency']");
+    private static SelenideElement processButton = $x("//button[@type= 'submit' and normalize-space()= 'Process']");
+    private static SelenideElement returnOnHomePageButton = $x("//button[@class ='btn home']");
 
     public BankManagerLoginPage addCustomerLoginButtonClick() {
         addCustomerLoginBtn.shouldBe(visible, Duration.ofSeconds(3)).click();
+        return this;
+    }
+
+    public BankManagerLoginPage fillFirstNameField(String text){
+        logger.info("Fill first name field");
+        firstNameField.setValue(text);
+        return this;
+    }
+
+    public BankManagerLoginPage fillLastNameField(String text){
+        logger.info("Fill last name field");
+        lastNameField.setValue(text);
+        return this;
+    }
+
+    public BankManagerLoginPage fillPostCode(String text){
+        logger.info("Fill post code field");
+        postCodeField.setValue(text);
+        return this;
+    }
+
+    public BankManagerLoginPage addCustomerConfirmButtonClick(){
+        logger.info("Confirming registration new customer");
+        addCustomerConfirmButton.click();
+        return this;
+    }
+
+    public BankManagerLoginPage openAccountButtonClick(){
+        openAccountButton.click();
+        return this;
+    }
+
+    public BankManagerLoginPage selectCustomerFieldClick(){
+        selectCustomerField.pressEnter();
+        return this;
+    }
+
+    public BankManagerLoginPage selectingCustomerFromList(String text){
+        selectCustomerField.selectOptionContainingText(text);
+        return this;
+    }
+
+    public BankManagerLoginPage selectCurrencyFieldClick(){
+        selectCurrency.click();
+        return this;
+    }
+
+    public BankManagerLoginPage selectingCurrencyFromList(String text){
+        selectCurrency.selectOptionContainingText(text);
+        return this;
+    }
+
+    public BankManagerLoginPage processButtonClick(){
+        processButton.click();
+        return this;
+    }
+
+    public BankManagerLoginPage returnOnHomePageClick(){
+        returnOnHomePageButton.click();
         return this;
     }
 }
--- src/test/java/tests/bankManager/BankManagerAccountTests.java
+++ src/test/java/tests/bankManager/BankManagerAccountTests.java
@@ -1,8 +1,11 @@
 package tests.bankManager;
 
 import baseClasses.BankManagerLoginPage;
+import baseClasses.CustomerLoginPage;
 import baseClasses.HomePage;
 import baseTest.AbstractBaseTest;
+import com.codeborne.selenide.Condition;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -10,6 +13,7 @@ public class BankManagerAccountTests extends AbstractBaseTest {
 
     private HomePage homePage;
     private BankManagerLoginPage bankManagerLoginPage;
+    private CustomerLoginPage customerLoginPage;
     private Assertions assertions;
 
 
@@ -17,8 +21,26 @@ public class BankManagerAccountTests extends AbstractBaseTest {
     public void openBankHomePage() {
         homePage = new HomePage();
         bankManagerLoginPage = new BankManagerLoginPage();
+        customerLoginPage = new CustomerLoginPage();
         assertions = new Assertions();
         homePage.open();
+    }
+    public void regNewCustomerAccount(String firstName, String lastName, String postCode, String nameOfSelectedToEnter, String currency){
+        homePage
+                .bankManagerLoginButtonClick();
+        bankManagerLoginPage
+                .addCustomerLoginButtonClick()
+                .fillFirstNameField(firstName)
+                .fillLastNameField(lastName)
+                .fillPostCode(postCode)
+                .addCustomerConfirmButtonClick()
+                .openAccountButtonClick()
+                .selectCustomerFieldClick()
+                .selectingCustomerFromList(nameOfSelectedToEnter)
+                .selectCurrencyFieldClick()
+                .selectingCurrencyFromList(currency)
+                .processButtonClick()
+                .returnOnHomePageClick();
     }
 
     @Test
@@ -41,5 +63,37 @@ public class BankManagerAccountTests extends AbstractBaseTest {
         bankManagerLoginPage.addCustomerLoginButtonClick();
         assertions.assertUrlContains("addCust");
     }
-
+    @Test
+    public void registrationNewCustomerTest1() {
+        regNewCustomerAccount("Josh","Browny", "E7777E", "Josh", "Pound" );
+        homePage
+                .customerLoginBtnClick();
+        customerLoginPage
+                .clickFieldSelectUserName()
+                .selectUserByName("Josh")
+                .clickBtnLogin();
+        Assert.assertEquals(customerLoginPage.getCurrencyValue().getText(), "Pound" );
+    }
+        @Test
+        public void registrationNewCustomerTest2() {
+            regNewCustomerAccount("James", "Browny", "E8888E", "James", "Dollar");
+            homePage
+                    .customerLoginBtnClick();
+            customerLoginPage
+                    .clickFieldSelectUserName()
+                    .selectUserByName("James")
+                    .clickBtnLogin();
+            Assert.assertEquals(customerLoginPage.getCurrencyValue().getText(), "Dollar" );
+        }
+        @Test
+        public void registrationNewCustomerTest3(){
+            regNewCustomerAccount("Jordan", "Browny", "E9999E", "Jordan", "Rupee");
+            homePage
+                    .customerLoginBtnClick();
+            customerLoginPage
+                    .clickFieldSelectUserName()
+                    .selectUserByName("Jordan")
+                    .clickBtnLogin();
+            Assert.assertEquals(customerLoginPage.getCurrencyValue().getText(), "Rupee" );
+    }
 }
